6825bb72e70802865a492440622eb90351503687
Jud <jud.howland@gmail.com>
2020-06-19 23:42:47 -0500

    added UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,7 +26,7 @@ $("#modalDueDate").datepicker( {
 });
 
 
-var auditTask = function (taskEl) {
+var auditTask = function (taskEl) {  
   // get date from task element
   var date = $(taskEl).find("span").text().trim();
 
@@ -43,6 +43,7 @@ var auditTask = function (taskEl) {
   else if (Math.abs(moment().diff(time, "days")) <= 2) {
     $(taskEl).addClass("list-group-item-warning");
   }
+  console.log(taskEl);
 };
 
 $(".card .list-group").sortable({
@@ -51,12 +52,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function (event) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function (event) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function (event) {
+    $(this).addClass("dropover-active");
   },
   out: function (event) {
+    $(this).removeClass("dropover-active");
   },
   update: function (event) {
     // array to store the data in
@@ -100,12 +107,15 @@ $("#trash").droppable({
   drop: function(event, ui) {
     console.log("drop");
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   over: function(event, ui) {
     console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
     console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -151,8 +161,7 @@ $(".list-group").on("click", "p", function () {
 $(".list-group").on("blur", "textarea", function () {
   // get the textarea's current value/text
   var text = $(this)
-    .val()
-    .trim();
+    .val();
 
   // get the parent ul's id attribute
   var status = $(this)
@@ -252,7 +261,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -284,5 +293,11 @@ $("#remove-tasks").on("click", function () {
 
 // load tasks for the first time
 loadTasks();
+
+setInterval(function() {
+  $(".card .list-group-item").each(function (el) {
+    auditTask($(this));
+  });
+}, 1800000);
 
 
